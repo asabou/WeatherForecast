b31f8e3847495d530fbd542629fe72d4b3415f01
adam <vargaadam23@yahoo.com>
2022-01-10 16:07:42 +0200

    Added working XGBoost

--- algorithm/XGBoost.py
+++ algorithm/XGBoost.py
@@ -4,28 +4,29 @@ from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import mean_squared_error
+import warnings
+warnings.filterwarnings("ignore")
 class XGBoost:
-    def __init__(self, trainInputs, trainOutputs, testInputs, testOutputs):
-        self.__trainInputs = trainInputs
-        self.__trainOutputs = trainOutputs 
-        self.__testInputs = testInputs 
-        self.__testOutputs = testOutputs
+    def __init__(self, x_train, y_train, x_test, y_test):
+       self.x_train, self.y_train, self.x_test, self.y_test = x_train, y_train, x_test, y_test
     
 
     def toolErrorPrediction(self):
         model = XGBClassifier()
-        model.fit(self.__trainInputs, self.__trainOutputs)
-        F = model.predict(self.__testInputs)
+        model.fit(self.x_train, self.y_train)
+        F = model.predict(self.x_test)
+        # predictions = [round(value) for value in F]
+
         print("F: ", F)
-        return mean_squared_error(self.__testOutputs,F)
+        return mean_squared_error(self.y_test,F)
 
     def accuracyTool(self):
         print("tool  : ", 100.0 - (self.toolErrorPrediction() * 100))
 
     def predictTool(self, data):
         model = XGBClassifier()
-        model.fit(self.__trainInputs, self.__trainOutputs)
-        F = model.predict(data)
+        model.fit(self.x_train, self.y_train)
+        F = model.predict(self.x_test)
         print("F tool:  ", F[0])
         if F[0] < 0.5:
             return "No"
@@ -33,17 +34,4 @@ class XGBoost:
 
 
 
-# model = XGBClassifier()
-# model.fit(X_train, y_train)
-
-# y_pred = model.predict(X_test)
-# predictions = [round(value) for value in y_pred]
-# #folosim XGBClassifier pentru predictii
-
-# accuracy = accuracy_score(y_test, predictions)
-# print("Accuracy: %.2f%%" % (accuracy * 100.0))
-# #masuram acuratetea (~85%)
-
-# print("Length of Training Data: {}".format(len(X_train)))
-# print("Length of Testing Data: {}".format(len(X_test)))
 
